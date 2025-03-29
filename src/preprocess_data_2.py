7cbf0062c20e79e037069e65d6c8eb43be94ff7e
a <andrew61@illinois.edu>
2025-03-28 21:41:34 -0500

    replaced string concatenation with bytesarray extend

--- src/preprocess_data_2.py
+++ src/preprocess_data_2.py
@@ -1,4 +1,6 @@
 import os
+import concurrent.futures
+import timeit
 
 # probably need to export these as environment variables in a module since I redefine them every script
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,31 +13,59 @@ RAW_TEXT_DIR = os.path.join(DATA_DIR, "raw")
 STAGE_1_PATH = os.path.join(DATA_DIR, "collection_stage_1.csv")
 STAGE_2_PATH = os.path.join(DATA_DIR, "collection_stage_2.csv")
 
-# idea is to read each book line by line but never load the entire text into memory at once
-# TODO: cleanup + parallelize (python not very good for this because of the global interpreter lock's mutex on io operations iirc)
+# NOTE: parallelizing naturally required books to have their full text loaded into memory as a given time.
 # TODO: nltk would be more robust likely, but remark we just need function words. (articles, pronouns, conjunctions, aux verbs, particles, etc.)
 
-with open(STAGE_2_PATH, "w") as s2f:
-    s2f.write("author,book,sequence\n")
-    with open(STAGE_1_PATH) as s1f:
-        next(s1f)
-        for line in s1f:
-            (a_id, t_id) = line.strip().split(",", 1)
-            book_path = os.path.join(RAW_TEXT_DIR, f"{a_id}_{t_id}.txt")
-            s2f.write(f"{a_id},{t_id},")
-            print(book_path)
-            with open(book_path, "r") as bf:
-                reading = False
-                for line in bf:
-                    line = line.strip().lower()
-                    if "***" in line:
-                        reading = not reading
-                    elif reading:
-                        term = ""
-                        for c in line + "\n":
-                            if c.isalpha():
-                                term += c
-                            else:
-                                if term != "":
-                                    s2f.write(term + " ")
-                                term = ""
+def parse_line(line):
+    subseq = bytearray()
+    term = bytearray()
+    for c in line + "\n":
+        if c.isascii():
+            term.append(ord(c))
+        else:
+            if term:
+                subseq.extend(term)
+                subseq.append(32)
+                term.clear()
+    return subseq
+
+def parse_book(record):
+    a_id, t_id = record
+    seq = bytearray()
+    book_path = os.path.join(RAW_TEXT_DIR, f"{a_id}_{t_id}.txt")
+    with open(book_path, "r") as bf:
+        reading = False
+        for line in bf:
+            line = line.strip().lower()
+            if "***" in line:
+                reading = not reading
+            elif reading:
+                subseq = parse_line(line)
+                seq.extend(subseq)
+    return seq.decode("ascii")
+
+def write_stage_2():
+    with open(STAGE_2_PATH, "w") as s2f:
+        s2f.write("author,book,sequence\n")
+        pool = []
+        with open(STAGE_1_PATH) as s1f:
+            next(s1f)
+            for line in s1f:
+                info = line.strip().split(",", 1)
+                pool.append(info)
+        # https://docs.python.org/3/library/concurrent.futures.html
+        with concurrent.futures.ProcessPoolExecutor() as exec:
+            for (a_id, t_id), seq in zip(pool, exec.map(parse_book, pool)):
+                s2f.write(f"{a_id},{t_id},{seq}\n")
+
+def main():
+    write_stage_2()
+    # test display since opening the file in your IDE will crash it...
+    # with open(STAGE_2_PATH, "r") as f:
+    #     for l in f:
+    #         l = l.strip()
+    #         print(l[:200])
+
+# need this guard for ProcessPoolExecutor
+if __name__ == "__main__":
+    main()
